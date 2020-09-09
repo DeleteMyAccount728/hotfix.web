0c2105a8961546517ee713041adbb5591270c396
Alexander Andreev <andr33v-ae@yandex.ru>
2020-09-09 23:41:45 +0300

    1 fix empty order creating

--- src/panels/Place.js
+++ src/panels/Place.js
@@ -101,7 +101,7 @@ const Place = ({ item, order, onIncrementPosition, onDecrementPosition, area }) 
         )))}
       </ul>
       <footer className="Place__footer">
-        <Link to={`/basket/${area.id}/${item.id}`} className="Place__order">
+        <Link to={`/basket/${area.id}/${item.id}`} className="Place__order" hidden={price === "0"}>
           Оформить заказ ({price})
         </Link>
       </footer>
